b1c2943529cbea25cc0997a70e9de3ee94600985
Anton Maklakov <anton@espressif.com>
2020-07-13 15:55:11 +0700

    wip

--- scripts/build/companion_libs/140-mpc.sh
+++ scripts/build/companion_libs/140-mpc.sh
@@ -111,11 +111,11 @@ do_mpc_backend() {
 
     #FIXME
     CT_DoExecLog EXTRA cp -av \
-        /home/mak/e/p/dyn/multilib1/gcc-xtensa-dynconfig-plugin/config-{esp32s2,esp32,esp8266}.so \
+        /home/mak/e/p/dyn/multilib1/gcc-xtensa-dynconfig-plugin/*-{esp32s2,esp32,esp8266}.so \
         "${prefix}/lib"
 
     CT_DoExecLog EXTRA cp -av \
-        /home/mak/e/p/dyn/multilib1/gcc-xtensa-dynconfig-plugin/config-{esp32s2,esp32,esp8266}.so \
+        /home/mak/e/p/dyn/multilib1/gcc-xtensa-dynconfig-plugin/*-{esp32s2,esp32,esp8266}.so \
         "${CT_PREFIX_DIR}/lib"
 }
 
